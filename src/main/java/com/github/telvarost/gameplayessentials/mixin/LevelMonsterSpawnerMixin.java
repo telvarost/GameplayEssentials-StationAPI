e52131353bd6386e8c764913b2e9624079151749
telvarost <telontelvarost@gmail.com>
2024-10-13 22:25:48 -0400

    Update to latest biny

--- src/main/java/com/github/telvarost/gameplayessentials/mixin/LevelMonsterSpawnerMixin.java
+++ src/main/java/com/github/telvarost/gameplayessentials/mixin/LevelMonsterSpawnerMixin.java
@@ -4,7 +4,7 @@ import com.github.telvarost.gameplayessentials.BedBehaviorEnum;
 import com.github.telvarost.gameplayessentials.Config;
 import java.util.List;
 
-import net.minecraft.class_567;
+import net.minecraft.world.NaturalSpawner;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,10 +12,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 
-@Mixin(class_567.class)
+@Mixin(NaturalSpawner.class)
 public class LevelMonsterSpawnerMixin {
 
-    @Inject(method = "method_1869", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "spawnMonstersAndWakePlayers", at = @At("HEAD"), cancellable = true)
     private static void nightmaresHandler(World level, List<PlayerEntity> playersList, CallbackInfoReturnable<Boolean> cir) {
         if (BedBehaviorEnum.DISABLE_NIGHTMARES == Config.config.BED_BEHAVIOR_ENUM) {
             cir.setReturnValue(false);
